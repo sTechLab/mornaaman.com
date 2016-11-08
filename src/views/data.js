4f448475279e5a83a066d40ddb902cfb0ff9b47a
mmoorr <mornaaman@yahoo.com>
2016-11-08 15:36:57 -0500

    Update data.js

--- src/views/data.js
+++ src/views/data.js
@@ -67,7 +67,7 @@ var publications = [
       author: 'Xiao Ma, Jeff Hancock, Kenneth Lim Mingjie, Mor Naaman ',
       source: 'CSCW 2017',
       sourceLink: 'https://cscw.acm.org/2017/',
-      paperLink: ''
+      paperLink: 'https://s.tech.cornell.edu/assets/papers/ma_airbnb.pdf'
     },
     {
       year: '2017',
